6ac0c9983d87a9f5e9afc101d21082688f71a811
JONATHAN MEYER <jonathan.meyer@libertymutual.com>
2020-04-26 06:44:59 -0400

    test update

--- app/containers/HomePage/messages.js
+++ app/containers/HomePage/messages.js
@@ -10,6 +10,6 @@ export const scope = 'app.containers.HomePage';
 export default defineMessages({
   header: {
     id: `${scope}.header`,
-    defaultMessage: 'Nerd.cafe',
+    defaultMessage: 'The Nerd Café',
   },
 });
